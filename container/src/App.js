ea57b06c7726a46a9de10eb030599f71e54d41a6
mvxj <mvxj11@gmail.com>
2024-04-06 14:57:18 +0200

    Prepare for Github actions

--- container/src/App.js
+++ container/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { mount } from 'marketing/MarketingApp';
-
+import MarketingApp from "./components/MarketingApp";
 export default () => {
-    return <h1>Hi there!</h1>
+    return <div>
+        <MarketingApp />
+    </div>
 }
